7073f6d0bd7dd1dd55e58e0f05129229f8e3932c
AshKemp <kmuraliashwin@gmail.com>
2023-05-08 16:34:21 +0530

    v2-greeting

--- src/functions/greeting/handler.ts
+++ src/functions/greeting/handler.ts
@@ -2,7 +2,7 @@ import { formatJSONResponse } from "@libs/api-gateway";
 
 const greeting = async (event) => {
   return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+    message: `Hello ${event.body.name}, welcome to the exciting Serverless world! This is version 2`,
     event,
   });
 };
